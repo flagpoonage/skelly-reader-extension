a5f0fe2b370d4d921c26eb768ae141eb940c4806
James Hay <pokinoice@live.com>
2023-07-27 08:57:15 +0800

    Gen keypair in service worker

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -103,3 +103,41 @@ extension.tabs.onUpdated.addListener((tabId, info) => {
     url: `${extension.runtime.getURL('reader.html')}?read=${url.href}`,
   });
 });
+
+(async () => {
+  const key = await globalThis.crypto.subtle.generateKey(
+    {
+      name: 'RSA-PSS',
+      hash: 'SHA-256',
+      modulusLength: 4096,
+      publicExponent: new Uint8Array([1, 0, 1]),
+    },
+    true,
+    ['sign', 'verify'],
+  );
+
+  function ab2str(buf: ArrayBuffer) {
+    return String.fromCharCode.apply(
+      null,
+      new Uint8Array(buf) as unknown as number[],
+    );
+  }
+
+  async function exportCryptoKey(type: 'public' | 'private', key: CryptoKey) {
+    const exported = await globalThis.crypto.subtle.exportKey(
+      type === 'public' ? 'spki' : 'pkcs8',
+      key,
+    );
+    const n = type.toUpperCase();
+    const exportedAsString = ab2str(exported);
+    const exportedAsBase64 = globalThis.btoa(exportedAsString);
+    const pemExported = `-----BEGIN ${n} KEY-----\n${exportedAsBase64}\n-----END ${n} KEY-----`;
+    return pemExported;
+  }
+
+  const pem_priv = await exportCryptoKey('private', key.privateKey);
+  const pem_pub = await exportCryptoKey('public', key.publicKey);
+
+  console.log(pem_priv);
+  console.log(pem_pub);
+})();
