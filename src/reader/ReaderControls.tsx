f4f3842281b83956a29bd448c5c1d400e4d3f176
James Hay <pokinoice@live.com>
2023-08-01 20:22:37 +0800

    Update reader controls

--- src/reader/ReaderControls.tsx
+++ src/reader/ReaderControls.tsx
@@ -19,6 +19,7 @@ export function ReaderControls() {
           <option value="esbuild">ESBuild</option>
           <option value="webpack">Webpack</option>
           <option value="overreacted">Overreacted</option>
+          <option value="rust">Rust</option>
         </select>
       </div>
     </footer>
