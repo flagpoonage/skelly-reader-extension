77605884e137828deae7e39ab82160c4490c2df8
Slobodan Jovanovski <slobodanjovanovski@yahoo.com>
2023-08-03 22:50:50 +0800

    Save toggle data in local storage

--- src/popup.tsx
+++ src/popup.tsx
@@ -2,9 +2,12 @@ import { renderElement } from './page-renderer';
 import './popup/popup.css';
 import { Toggle } from "./components/Toggle";
 import { Button } from "./components/Button";
+import { Select } from "./components/Select";
+import { themes } from "./common/themes";
+import theme from "tailwindcss/defaultTheme";
 
 function Popup() {
-  const onClick = async () => {
+  const handleViewInSkellyClick = async () => {
     const app = chrome.runtime.getURL('reader.html');
     const current_window = await chrome.windows.getCurrent();
 
@@ -21,13 +24,31 @@ function Popup() {
     const first_tab_id = tabs?.[0]?.id;
 
     if (typeof first_tab_id !== 'number') {
-      chrome.tabs.create({ url: `${app}#${active_tab.url}` });
+      chrome.tabs.create({url: `${app}#${active_tab.url}`});
     } else {
       chrome.tabs.update(first_tab_id, {
         active: true,
       });
     }
   };
+
+  const handleCryptoClick = () => chrome.tabs.create({url: `${chrome.runtime.getURL('crypto.html')}`})
+
+  const handleToggleImages = (enabled: boolean) => {
+    localStorage.setItem('displayDefaultImages', JSON.stringify(enabled))
+  }
+
+  const handleToggleSVG = (enabled: boolean) => {
+    localStorage.setItem('displayDefaultSVG', JSON.stringify(enabled))
+  }
+
+  const handleToggleVideos = (enabled: boolean) => {
+    localStorage.setItem('displayDefaultVideos', JSON.stringify(enabled))
+  }
+
+  const displayDefaultImages = localStorage.getItem("displayDefaultImages")
+  const displayDefaultSVG = localStorage.getItem("displayDefaultSVG")
+  const displayDefaultVideos = localStorage.getItem("displayDefaultVideos")
 
   return (
     <div className='w-[350px] p-4'>
@@ -36,21 +57,39 @@ function Popup() {
           Skelly
         </div>
         <Button className='bg-purple-950 text-white'>
-          Donate
+          $Donate$
         </Button>
       </div>
       <div className='grid gap-4'>
-        <Toggle text='Load images'/>
-        <Toggle text='Load videos'/>
+        <Toggle
+          title='Display images'
+          description='Default value for all pages open in Skelly'
+          onToggle={handleToggleImages}
+          defaultValue={JSON.parse(displayDefaultImages ?? 'false')}
+        />
+        <Toggle
+          title='Display SVG'
+          description='Default value for all pages open in Skelly'
+          onToggle={handleToggleSVG}
+          defaultValue={JSON.parse(displayDefaultSVG ?? 'true')}
+        />
+        <Toggle
+          title='Display videos'
+          description='Default value for all pages open in Skelly'
+          onToggle={handleToggleVideos}
+          defaultValue={JSON.parse(displayDefaultVideos ?? 'false')}
+        />
+        <div>
+          <label className="text-sm mb-1 block font-medium text-gray-900">Select default theme</label>
+          <Select options={themes} defaultValue='none'/>
+        </div>
         <div className='flex justify-between'>
-          <Button onClick={onClick} className='bg-emerald-900 text-white'>
+          <Button onClick={handleViewInSkellyClick} className='bg-emerald-900 text-white'>
             View in Skelly
           </Button>
           <Button
             className='bg-emerald-300'
-            onClick={() =>
-              chrome.tabs.create({url: `${chrome.runtime.getURL('crypto.html')}`})
-            }
+            onClick={handleCryptoClick}
           >
             Open Crypto
           </Button>
